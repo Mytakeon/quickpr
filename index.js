49259b71c07f49747861384c41d8b786700b3e94
Arthur Alglave <aalglave@microsoft.com>
2022-05-10 16:48:42 +0100

    Fix getPRUrl ADO

--- index.js
+++ index.js
@@ -43,15 +43,15 @@ const quickGitPr = () => {
     execSync(`git push --set-upstream ${remoteName} ${branchName}`);
 
     const remoteUrl = execSync(`git remote get-url ${remoteName}`).toString().trim();
-    execSync(`start ${getPRUrl(remoteUrl, branchName)}`);
+    execSync(`start ${getPRUrl(remoteUrl, branchName, remoteName)}`);
 }
 
-const getPRUrl = (remoteUrl, sourceBranch) => {
+const getPRUrl = (remoteUrl, sourceBranch, remoteName) => {
 
     if (remoteUrl.includes('github.com') || remoteUrl.includes('ghe.com')) { // Github, Github Enterprise
         // take the .git away from the remoteUrl
         return `${remoteUrl.slice(0, -4)}/compare/${sourceBranch}?expand=1`;
-    } else if (remoteUrl.indexOf('visualstudio.com') > -1) { // Azure DevOps
+    } else if (remoteUrl.includes('visualstudio.com')) { // Azure DevOps
         const targetBranch = execSync(`git rev-parse --abbrev-ref ${remoteName}/HEAD`).toString().split("/")[1].trim();
         console.log(`Using target branch '${targetBranch}'`);
         return `${remoteUrl}/pullrequestcreate?sourceRef=${sourceBranch}&targetRef=${targetBranch}`;
